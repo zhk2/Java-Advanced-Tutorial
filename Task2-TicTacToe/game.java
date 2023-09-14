b14787fb34edbb5137b9e18d0f81cfb365ddcc3f
Sky Casey <skythecreative@gmail.com>
2023-09-14 09:09:25 -0700

    Create game.java

---
+++ Task2-TicTacToe/game.java
@@ -0,0 +1,37 @@
+/*
+ * ## TicTacToe
+
+# TASK : Make a game of Tic Tac Toe in Java using the terminal! Start with Player1 , who will place down an ‘X’ and then Player2 will place down an ‘O’. The player input will loop until a win condition is found.
+
+- **`do-while`** loop - ( for player input)
+- **`for`** loop - (iterating through array)
+- methods - (functions to make the game easier to understand)
+- multidimensional arrays - (store game data)
+- player id - (determine which icon to use for play)
+
+Input ( ROW # )
+
+Input ( COLUMN # )
+
+### Methods
+
+- SetCell( playerID , row , column )
+- Check Win()
+    - Check Row
+    - Check Column
+    - Check Diagonal
+ * 
+ * 
+ */
+
+
+
+
+class Game {
+
+    public void main (String[] args)
+    {
+        
+    }
+
+}
